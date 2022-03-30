fb50501e4145f083d695ced76462f42da3c9ca4d
Blake Beckstrom <dabeck88@gmail.com>
2022-03-30 12:29:22 -0400

    created navbar

---
+++ my-app/src/components/nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Nav() {
+	return (
+		<div className="navbar">
+			<a href="#introduction" class="navbar__link">
+				Introduction
+			</a>
+			<a href="#vision" class="navbar__link">
+				Vision
+			</a>
+			<a href="#Build" class="navbar__link">
+				Introduction
+			</a>
+			<a href="#finalproduct" class="navbar__link">
+				Final Product
+			</a>
+		</div>
+	);
+}
