0406f5abd3a1eea6796c624bb80b4cbbf0978fcb
Blake Beckstrom <dabeck88@gmail.com>
2022-03-30 12:29:35 -0400

    created index.js

---
+++ my-app/src/components/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { UserContext, Route, Routes, HashRouter } from "./context";
+import Home from "./home";
+import Nav from "./nav";
+
+export default function Spa() {
+	return (
+		<HashRouter>
+			<UserContext.Provider>
+				<Nav />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/home/" element={<Home />} />
+				</Routes>
+			</UserContext.Provider>
+		</HashRouter>
+	);
+}
